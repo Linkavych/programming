5648710ad44d2a0d90cbd4a86564088deb66f09b
Adam V. Link <linkavych@gmail.com>
2021-10-24 12:17:00 -0400

    Ran through black and pylint

--- python/Red_Siege/ip_geo.py
+++ python/Red_Siege/ip_geo.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 # Geolocation and ownership of a given IP address or network
 # This time using an API and requests
-import requests
 import sys
-import json
 import ipaddress
-from pprint import pprint
+import requests
 
-API_KEY = ''
+API_KEY = ""
+
 
 def usage():
+    """
+    Usage function
+    Prints usage for the program
+    """
     print("Usage: ./ip_geo.py <api_key_file> <ip file> <output_file>")
-    exit(1)
+    sys.exit(1)
+
 
 def lookup(ip_addr, API_KEY):
-    '''
+    """
     Requests an ip lookup from  app.geolocation.io
     Params:
         ip_addr - a single ip address
@@ -22,20 +26,19 @@ def lookup(ip_addr, API_KEY):
     Return:
         name - owner of ip address
         country - country code
-    '''
-    payload = {'apiKey': API_KEY, 'ip': ip_addr, 'fields': 'organization,country_code3'}
-    r = requests.get('https://api.ipgeolocation.io/ipgeo', params=payload)
+    """
+    payload = {"apiKey": API_KEY, "ip": ip_addr, "fields": "organization,country_code3"}
+    r = requests.get("https://api.ipgeolocation.io/ipgeo", params=payload)
 
     data = r.json()
-#    pprint(data)
-    country = data['country_code3']
-    name = data['organization']
+    country = data["country_code3"]
+    name = data["organization"]
 
     return name, country
 
 
 def main(infile, outfile):
-    '''
+    """
     Main function for program to verify ownership and geolocation
     of a given IP address or range.
 
@@ -43,11 +46,11 @@ def main(infile, outfile):
         infile - a file with a list of IP addresses, or CIDR ranges
     Returns:
         outfile - new file list ip, owner, and country code
-    '''
+    """
     ip_info = []
     ip_list2 = []
 
-    with open(infile, 'r') as f:
+    with open(infile, "r", encoding="utf-8") as f:
         ips = f.readlines()
 
     for ip in ips:
@@ -55,7 +58,7 @@ def main(infile, outfile):
         try:
             ip_addr = ipaddress.ip_address(ip)
         except ValueError:
-            print("[!] IP: {} is not a valid IP.".format(ip))
+            print(f"[!] IP: {ip} is not a valid IP.")
 
             try:
                 ip_list2 = [str(ip1) for ip1 in ipaddress.ip_network(ip, strict=False)]
@@ -65,26 +68,26 @@ def main(infile, outfile):
                     name, country = lookup(ip2, API_KEY)
                     ip_info.append((name, country, ip2))
             except ValueError:
-                print("[!] Not a valid IP range: {}".format(ip2))
+                print(f"[!] Not a valid IP range: {ip2}")
                 continue
             continue
 
         name, country = lookup(ip_addr, API_KEY)
         ip_info.append((name, country, ip_addr))
 
-    with open(outfile, 'w') as fd:
+    with open(outfile, "w", encoding="utf-8") as fd:
         for item in ip_info:
-            fd.write('Owner: {}, Country: {}, IP: {}\n'.format(item[0], item[1], item[2]))
+            fd.write(f"Owner: {item[0]}, Country: {item[1]}, IP: {item[2]}\n")
 
-    print("[+] File written to: {}".format(outfile))
+    print(f"[+] File written to: {outfile}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         usage()
     else:
-        with open(sys.argv[1], 'r') as fd:
+        with open(sys.argv[1], "r", encoding="utf-8") as fd:
             API_KEY = fd.readline()
             API_KEY = API_KEY.strip()
 
--- python/Red_Siege/ip_whois.py
+++ python/Red_Siege/ip_whois.py
@@ -8,38 +8,43 @@ from ipwhois import IPWhois
 
 
 def usage():
+    """
+    Usage function.
+    Prints usage to stdout.
+    """
     print("Usage: ./ip_whois.py <ip file> <outfile>")
-    exit(1)
+    sys.exit(1)
+
 
 def lookup(ip_addr):
-    '''
+    """
     Conduct the actual lookup of the ip address with whois
     Params:
         ip_addr - an IP address
     Return:
         Owner and geolocation
-    '''
+    """
     ip_obj = IPWhois(ip_addr)
 
     info = ip_obj.lookup_rdap(depth=1)
 
     country = info["asn_country_code"]
-    name = info["network"]['name']
+    name = info["network"]["name"]
 
     return name, country
 
-def main(filename, output_file):
 
-    '''
+def main(filename, output_file):
+    """
     Main function for the program
     Param:
         filename - name of file to be read
     Return:
         new file - with IP address - Country - Owner
-    '''
+    """
     info = []
     ip_list = []
-    with open(filename, 'r') as f:
+    with open(filename, "r", encoding="utf-8") as f:
         ips = f.readlines()
 
     for ip in ips:
@@ -47,7 +52,7 @@ def main(filename, output_file):
         try:
             ip_addr = ipaddress.ip_address(ip)
         except ValueError:
-            print("[!] IP: {} is not valid.".format(ip))
+            print(f"[!] IP: {ip} is not valid.")
             try:
                 ip_list = [str(ip_a) for ip_a in ipaddress.ip_network(ip, strict=False)]
                 for ip2 in ip_list:
@@ -56,20 +61,21 @@ def main(filename, output_file):
                     name, country = lookup(ip2)
                     info.append((ip2, name, country))
             except ValueError:
-                print("[!] Not a valid IP range: {}".format(ip))
+                print(f"[!] Not a valid IP range: {ip}")
                 continue
             continue
 
         name, country = lookup(ip_addr)
         info.append((ip_addr, name, country))
 
-    with open(output_file, 'a') as fd:
+    with open(output_file, "a", encoding="utf-8") as fd:
         for item in info:
-            fd.write('Owner: {}, Country: {}, IP: {}\n'.format(item[1], item[2], item[0]))
+            fd.write(f"Owner: {item[1]}, Country: {item[2]}, IP: {item[0]}\n")
 
-    print("[+] File located at: {}".format(output_file))
+    print(f"[+] File located at: {outfile}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) != 3:
         usage()
     else:
@@ -77,7 +83,3 @@ if __name__ == '__main__':
         output_file = sys.argv[2]
 
     main(filename, output_file)
-
-
-
-
