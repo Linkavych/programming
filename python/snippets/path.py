ad7edcdc3d6a1c4789f79ab7f76616cd1e16fc78
Adam V. Link <linkavych@gmail.com>
2022-08-12 07:50:52 -0400

    Adding snippets

---
+++ python/snippets/path.py
@@ -0,0 +1,46 @@
+import pathlib
+import sys
+
+cwd = pathlib.Path.cwd()
+home = pathlib.Path.home()
+user_dir = pathlib.Path(sys.argv[1])
+
+def tree(directory):
+    '''
+    Print a directory tree based on the directory input
+
+    Always recursive
+    '''
+    print(f'+ {directory}')
+    for path in sorted(directory.rglob('*')):
+        depth = len(path.relative_to(directory).parts)
+        spacer = '    ' * depth
+        print(f'{spacer}+ {path.name}')
+
+def file_list(directory):
+    '''
+    Get a list of files from a directory
+
+    Returns a list of files, sorted
+    '''
+    files = sorted(directory.glob('*'))
+
+    return files
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    files = file_list(user_dir)
+    for file in files:
+        if file.is_dir():
+            print(f'Directory: {file}')
+        else:
+            print(f'File: {file}')
