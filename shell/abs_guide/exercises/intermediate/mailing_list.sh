ac0a8e2fee8da93f4d9290462a70cf7fce9b41e4
Adam V. Link <linkavych@gmail.com>
2023-02-22 20:49:01 -0500

    scripts

---
+++ shell/abs_guide/exercises/intermediate/mailing_list.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# A script to manage a mailing list with mail
+# Sends an email monthly (cron), the body is from a specified text file
+# The email list is also from a text file
+# 
+# author: Adam V. Link (@linkavych)
+# date: 2022-11-25
+#
+LIST=/tmp/mailer.list
+MESSAGE=/tmp/message.txt
+NEWSLOG=/tmp/mailer.log
+DATE=$(date "+%Y.%m.%d")
+
+for email in $(cat "$LIST")
+do
+    mail -s "Monthly Newsletter - [$DATE]" "$email" < "$MESSAGE"
+    echo "[+] Message sent to $email - [$DATE]" > "$NEWSLOG"
+done
+
